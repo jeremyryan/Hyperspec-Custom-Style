21683748573991377b4c23e9129105478a03c8c0
Jeremy Ryan <jeremyryan000@gmail.com>
2013-12-11 07:05:53 -0500

    added glossary server and updated db

---
+++ HyperSpecGlossary.lisp
@@ -0,0 +1,71 @@
+
+
+(ql:quickload '(:clack :cl-who :kmrcl :sqlite :yason))
+
+(import '(clack:call clack:<component> kmrcl:split-uri-query-string))
+
+(defvar *db-location* "/home/jmr/github/Hyperspec-Custom-Style/hyperspec.sqlite")
+
+(defvar *db* (sqlite:connect *db-location*))
+
+(defvar *prepared-stmt* (sqlite:prepare-statement *db*
+						  "select 
+  key,
+  term,
+  definition
+from glossary
+where key = ?"
+						  ))
+
+
+
+(defun gen-response (env)
+  (loop
+     with result = nil
+     with indexes = (loop for i from 0 to 2 collect i)
+     for (key . val) in (split-uri-query-string 
+			 (getf env :http-posted-content))
+     when (string= key "d%5B%5D")
+     do (progn
+	  (sqlite:reset-statement *prepared-stmt*)
+	  (sqlite:bind-parameter *prepared-stmt* 1 val)
+	  (when (sqlite:step-statement *prepared-stmt*)
+	    (push (mapcar (lambda (n)
+			    (sqlite:statement-column-value *prepared-stmt* n))
+			  indexes)
+		  result)))
+     finally (return (with-output-to-string (s)
+		       (yason:encode result s)))))
+
+(defclass <test-app> (<component>) ())
+
+(defmethod call ((this <test-app>) env)
+  `(200
+    (:content-type "application/json")
+    (,(gen-response env))))
+
+(defvar clack-test-server)
+
+(setf clack-test-server
+  (clack:clackup (make-instance '<test-app>)
+		 :server :apache
+		 :port 4040)) 
+
+
+(defun generate-html (env)
+  (let ((query-params (split-uri-query-string 
+		       (getf env :http-posted-content))))
+    (who:with-html-output-to-string (s)
+      (:html 
+       (:head 
+	(:title "test")
+	#|
+	(:link :rel "stylesheet"
+	:type "text/css"
+	:href "resources/styles.css"))
+	|#
+	(:body 
+	 (:div 
+	  (who:fmt "<p>~A</p>" query-params)
+	  (:table 
+	   (who:fmt "~{<tr><td>~A:</td> <td>~A</td></tr>~}" env)))))))))

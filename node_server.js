ce63f651b469db62a5fd8dec45c319a0b6b80953
Jeremy Ryan <jeremyryan000@gmail.com>
2014-03-03 04:08:51 -0500

    updated node_server.js

--- node_server.js
+++ node_server.js
@@ -1,12 +1,24 @@
 var sqlite = require('sqlite3');
 var http = require('http');
+var url = require('url');
 
-//var db = new sqlite.Database("hyperspec.sqlite");
+var db = new sqlite.Database("hyperspec.sqlite");
+var sql = "select key, term, definition from glossary where key = ?";
+var stmt = db.prepare(sql);
 
 http.createServer(function(request, response) { 
-    console.log(request);
+    var terms = [];
+    var query = url.parse(request, true).query;
+
+    for (var k in Object.keys(query)) {
+	stmt.reset();
+	stmt.get(query[k], function(e, row) { 
+	    terms.push(row);
+	});
+    }
+
     response.writeHead(200, { 'Content-Type': 'application/json' });
-    response.end(JSON.stringify({ lol: 'dongs' }));
+    response.end(JSON.stringify(terms));
 }).listen(8899);
 
-//console.log(sqlite);
+

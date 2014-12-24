2c715e7e91b9d4b5e8f01287cf9764c4efa1ba57
jeremyryan <jeremyryan000@gmail.com>
2014-12-24 03:56:59 -0500

    Added ability to view a listing of keyboard shortcuts by holding down Ctl-Alt

--- Hyperspec.user.js
+++ Hyperspec.user.js
@@ -35,10 +35,10 @@
 
     // Add a definition, or grab the page on which it is defined if it hasn't yet been loaded
     function addDef($a, promise) {
-        var result = promise;
-        var href = $a.attr('href');
-        var key  = href.substring(href.indexOf('#') + 1, href.length);
-        var definition = localStorage.getItem(key);
+        var result = promise,
+            href = $a.attr('href'),
+            key  = href.substring(href.indexOf('#') + 1, href.length),
+            definition = localStorage.getItem(key);
         if (definition) {
             $a.attr('title', definition);
         } else {
@@ -55,34 +55,49 @@
 
     // Maps characters to urls
     var charToUrlMap = {
-	I: '../Front/index.htm', // Top level index
-	L: '../Front/Hilights.htm', // Selected Highlights
-	C: '../Front/Contents.htm', // Chapter Index
-	M: '../Front/X_Master.htm', // Master Index
-	S: '../Front/X_Symbol.htm', // Symbol Index
-	G: '../Body/26_a.htm', // Glossary
-	X: '../Front/X3J13Iss.htm', // X3J13 Issue Index
-	E: '../Front/Help.htm' // Help, Legalese, Trivia, etc.
+        I: { url: '../Front/index.htm',    display: 'Top level index' },
+        L: { url: '../Front/Hilights.htm', display: 'Selected Highlights' },
+        C: { url: '../Front/Contents.htm', display: 'Chapter Index' },
+        M: { url: '../Front/X_Master.htm', display: 'Master Index' }, 
+        S: { url: '../Front/X_Symbol.htm', display: 'Symbol Index' },
+        G: { url: '../Body/26_a.htm',      display: 'Glossary' },
+        X: { url: '../Front/X3J13Iss.htm', display: 'X3J13 Issue Index' },
+        E: { url: '../Front/Help.htm',     display: 'Help, Legalese, Trivia, etc.' } 
     };
 
     // Add definitions as tooltips to all linked glossary terms
     function init() {
-	// set the page specific keys
-	$.extend(charToUrlMap, {
-	    J: $('a[rel=PREV]').attr('href'),
-	    K: $('a[rel=UP]').attr('href'),
-	    L: $('a[rel=NEXT]').attr('href')
-	});
-        var $anchors = $('a[rel=DEFINITION][href^=26_glo_], a[rel=DEFINITION][href^=#]');
-        var promise = $.Deferred().resolve().promise();
-	$anchors.each(function(idx, anchor) { 
-	    promise = addDef($(anchor), promise);
-	});
+        var $anchors = $('a[rel=DEFINITION][href^=26_glo_], a[rel=DEFINITION][href^=#]'),
+            promise = $.Deferred().resolve().promise(),
+            $menuList = $('<ul></ul>'),
+            $menuDiv = $('<div id="menu"></div>');
+        // set the page specific keys
+        $.extend(charToUrlMap, {
+            J: { url: $('a[rel=PREV]').attr('href'), display: 'Previous' },
+            K: { url: $('a[rel=UP]').attr('href'),   display: 'Up' },
+            L: { url: $('a[rel=NEXT]').attr('href'), display: 'Next' } 
+        });
+        $(Object.keys(charToUrlMap).sort()).each(function(_, key) {
+            var map = charToUrlMap[key];
+            $menuList.append($('<li></li>').text(key + ': ' + map.display));
+        });
+        $('body').append($menuDiv.append($menuList));
+        $anchors.each(function(idx, anchor) { 
+            promise = addDef($(anchor), promise);
+        });
         $(document).tooltip().on('keyup', function(e) { 
-	    if (!(e.altKey && e.ctrlKey)) return;
-	    var url = charToUrlMap[String.fromCharCode(e.keyCode)];
-	    if (url) window.location = url;
-	});
+            if (e.altKey && e.ctrlKey) {
+                var key = String.fromCharCode(e.keyCode),
+                    urlMap = charToUrlMap[key]; 
+                if (urlMap && urlMap.url) window.location = urlMap.url;
+            } else {
+                $menuDiv.css('display', 'none');
+            }
+        }).on('keydown', function(e) {
+            if (e.altKey && e.ctrlKey) {
+                $menuDiv.css('display', 'block');
+            } 
+        });
     }
 
     //  Bootstrap by loading jQuery and jQuery UI
